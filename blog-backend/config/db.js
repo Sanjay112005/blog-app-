b469a44b502a0e1385820214f645a256c1a863f2
Sanjay <147521217+Sanjay112005@users.noreply.github.com>
2025-07-03 19:01:10 +0530

    Update db.js

--- blog-backend/config/db.js
+++ blog-backend/config/db.js
@@ -1,12 +1,20 @@
-  
 const { Pool } = require("pg");
 require("dotenv").config();
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
 
+const isProduction = process.env.NODE_ENV === "production";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction
+    ? {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    : false,
 });
 
 pool.connect()
-.then(()=> console.log("Connected to the database"))
-.catch((err) => console.error("Connection error", err.stack));
-module.exports = pool
+  .then(() => console.log("Connected to the database"))
+  .catch((err) => console.error("Connection error", err.stack));
+
+module.exports = pool;
